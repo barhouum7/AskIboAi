aaefb9ccc806d1ca2a0553073aa26ddb0b037dfb
barhouum7 <bb.med2000@gmail.com>
2022-12-30 12:04:18 +0100

    Feat: Fetching the data from the server

--- client/script.js
+++ client/script.js
@@ -79,6 +79,36 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
 
   loader(messageDiv);
+
+  // Fetch the data from server -> Bot's Response...
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    console.log({parsedData});
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong!";
+
+    alert(err);
+  }
+  
 }
 
 form.addEventListener('submit', handleSubmit);
