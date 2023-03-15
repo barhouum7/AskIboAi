4318e75aef5d703d3c68eaad8b2a0cede1d2c159
barhouum7 <bb.med2000@gmail.com>
2023-03-15 21:32:28 +0100

    Ref: Updated Ai model settings

--- server/server.js
+++ server/server.js
@@ -56,7 +56,7 @@ app.post('/', async (req, res) => {
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${prompt}`,
-            temperature: 0,
+            temperature: 0.7,
             max_tokens: 3000,
             top_p: 1,
             frequency_penalty: 0.5,
